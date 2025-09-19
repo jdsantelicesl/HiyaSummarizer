c8519368df755bc8d5db61718e825c8f97ea27ce
jdsantelicesl <jdsantelicesl@gmail.com>
2025-09-19 14:41:37 -0700

    transcribes with floating file bug

--- backend/app.py
+++ backend/app.py
@@ -10,9 +10,7 @@ import textwrap
 connected = set()
 
 load_dotenv()
-client = OpenAI(
-    api_key=os.getenv("openAI_key")
-)
+client = OpenAI(api_key=os.getenv("openAI_key"))
 
 
 async def handler(ws):
@@ -24,33 +22,49 @@ async def handler(ws):
             print("Received: ", data.get("type"))
 
             if data.get("type") == "Stop":
-                for c in connected:
-                    if c != ws:
-                        await c.send(json.dumps({"type": "Stop"}))
-                await ws.close(code=1000, reason="Manual closure requested")
-                print("WebSocket closed manually")
 
                 with open("chunk.webm", "rb") as audio_file:
-                    transcript = client.audio.transcriptions.create(model="whisper-1", file=audio_file)
+                    transcript = client.audio.transcriptions.create(
+                        model="whisper-1", file=audio_file
+                    )
 
                     response = client.chat.completions.create(
                         model="gpt-4o-mini",
                         messages=[
-                            {"role": "system", "content": "Give a brief summary of the text provided and create a todo list with action items. Add to the todo list only items that were mentioned, dont speculate."},
+                            {
+                                "role": "system",
+                                "content": "Give a brief summary of the text provided and create a todo list with action items. Add to the todo list only items that were mentioned, dont speculate.",
+                            },
                             {
                                 "role": "user",
                                 "content": transcript.text,
                             },
                         ],
                     )
-                    parsed_response = textwrap.indent(response.choices[0].message.content, "- ")
+                    parsed_response = textwrap.indent(
+                        response.choices[0].message.content, "- "
+                    )
                     print(parsed_response)
+
+                await ws.send(
+                    json.dumps(
+                        {
+                            "type": "Summary",
+                            "payload": response.choices[0].message.content,
+                        }
+                    )
+                )
+
+                for c in connected:
+                    if c != ws:
+                        await c.send(json.dumps({"type": "Stop"}))
+                await ws.close(code=1000, reason="Manual closure requested")
+                print("WebSocket closed manually")
 
                 if os.path.exists("chunk.webm"):
                     os.remove("chunk.webm")
-                    
 
-            else: 
+            else:
                 # append to audio to file
                 audio_bytes = base64.b64decode(data["payload"])
                 with open("chunk.webm", "ab") as f:
@@ -64,6 +78,7 @@ async def handler(ws):
 
     finally:
         connected.remove(ws)
+
 
 async def main():
     async with websockets.serve(handler, "0.0.0.0", 5000):
